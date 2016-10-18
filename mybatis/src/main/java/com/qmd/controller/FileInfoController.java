89216fd8894aaec12e252186d0b2b0b94340ec1d
luxiang <luxiang@luxiang-PC>
2016-10-18 11:02:45 +0800

    lx

--- mybatis/src/main/java/com/qmd/controller/FileInfoController.java
+++ mybatis/src/main/java/com/qmd/controller/FileInfoController.java
@@ -7,19 +7,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.qmd.model.FileInfo;
-import com.qmd.serviceimpl.FileInfoServiceImpl;
+import com.qmd.service.FileInfoService;
 
 @Controller
 @RequestMapping("/qmd")
 public class FileInfoController {
 
 	@Autowired
-	FileInfoServiceImpl fileInfoServiceImpl;
+	FileInfoService fileInfoService;
 
 	@RequestMapping("/fy")
-	public String showFileInfo(int fileId, HttpServletRequest request) {
+	public String showFileInfo(HttpServletRequest request) {
 		System.out.println("=====================");
-		FileInfo fileInfo = fileInfoServiceImpl.getFileInfoById(fileId);
+		// FileInfo fileInfo = fileInfoServiceImpl.getFileInfoById(fileId);
+
+		FileInfo fileInfo = new FileInfo();
+		fileInfo.setFileCategory("专利类");
+		fileInfo.setFileName("哈利波特");
+		
+		
 		request.setAttribute("fileInfo", fileInfo);
 		return "showFileInfo";
 		// System.out.println;
