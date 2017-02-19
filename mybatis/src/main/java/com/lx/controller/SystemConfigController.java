05eab92f52bac42157fd6b2354563ceb702e5354
Administrator <Administrator@192.168.0.106>
2017-02-19 21:39:50 +0800

    1

--- mybatis/src/main/java/com/lx/controller/SystemConfigController.java
+++ mybatis/src/main/java/com/lx/controller/SystemConfigController.java
@@ -3,20 +3,25 @@ package com.lx.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 @Controller
 @RequestMapping("/SystemConfigController")
 public class SystemConfigController {
 
 	private static Logger logger = Logger.getLogger(SystemConfigController.class);
-
 	// 备份
 	@RequestMapping("/backup")
+	@ResponseBody
 	public Map<String, Object> backup(HttpServletRequest request) {
 		logger.info("=================backup==================");
 		Map<String, Object> map = new HashMap<String, Object>();
@@ -31,6 +36,7 @@ public class SystemConfigController {
 
 	// 还原
 	@RequestMapping("/restore")
+	@ResponseBody
 	public Map<String, Object> restore(HttpServletRequest request) {
 		logger.info("=================restore==================");
 		Map<String, Object> map = new HashMap<String, Object>();
@@ -44,6 +50,7 @@ public class SystemConfigController {
 	}
 	//初始化索引
 	@RequestMapping("/build")
+	@ResponseBody
 	public Map<String, Object> build(HttpServletRequest request) {
 		logger.info("=================restore==================");
 		Map<String, Object> map = new HashMap<String, Object>();
